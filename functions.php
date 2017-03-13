9ec83cd853ce8f82c5d2dbc1e896cdb479dde620
Jack Whiting <hi@jackwhiting.co.uk>
2017-03-13 11:53:31 +0000

    Gulp test

--- functions.php
+++ functions.php
@@ -4,5 +4,6 @@
  * Enqueue and register scripts the right way.
  */
 add_action('wp_enqueue_scripts', function () {
-    // wp_enqueue_style('base-theme', get_theme_file_uri() . '/css/main.css', [], '', 'all');
+    // wp_enqueue_style('build', get_theme_file_uri() . '/css/main.css', [], '', 'all');
+    // wp_enqueue_script('build', get_theme_file_uri() . '/js/min/build.js', ['jquery']);
 });
