a45f5a985b572be15940f12a7d70ceb73617f8d9
Jack Whiting <hi@jackwhiting.co.uk>
2017-06-28 14:34:03 +0100

    favicon

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/png" href="/logo.png"/>
 
     <?php wp_head(); ?>
 
